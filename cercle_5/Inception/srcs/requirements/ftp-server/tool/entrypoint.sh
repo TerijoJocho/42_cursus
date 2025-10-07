2b6c73dfabb30dbd6bd9f2aa4dba17e377f77eec
Daryl Avril <daavril@f1r11s16.clusters.42paris.fr>
2025-10-07 15:36:14 +0200

    add Inception done

--- cercle_5/Inception/srcs/requirements/ftp-server/tool/entrypoint.sh
+++ cercle_5/Inception/srcs/requirements/ftp-server/tool/entrypoint.sh
@@ -4,23 +4,27 @@ set -e
 
 FTP_USER=${FTP_USER:-ftp_user}
 FTP_PASSWORD=$(cat /run/secrets/ftp_password)
+FTP_HOME=/var/www/html/ftp_uploads
 
 #creer un user sans mdp, definie son home et l'interdit de se co avec un shell
 #-> user que pour ftp qui pointe vers le site wp
 if ! id "$FTP_USER" &>/dev/null 2>&1; then
-	adduser -D -h /var/www/html -s /bin/false "$FTP_USER"
+	adduser -D -h "$FTP_HOME" -s /bin/false "$FTP_USER"
 fi
+
+mkdir -p "$FTP_HOME"
+chown -R "$FTP_USER:$FTP_USER" "$FTP_HOME"
+chmod 755 "$FTP_HOME"
 
 #on lit le mdp et on l'injecte dans chpasswd
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
 #permissions pour que ftp_user puisse écrire
-chown -R "$FTP_USER:$FTP_USER" /var/www/html
-find /var/www/html -type d -exec chmod 755 {} \;
-find /var/www/html -type f -exec chmod 644 {} \;
+find "$FTP_HOME" -type d -exec chmod 755 {} \;
+find "$FTP_HOME" -type f -exec chmod 644 {} \;
 
 # Créer le répertoire pour vsftpd
 mkdir -p /var/run/vsftpd/empty
 
 #on remplace le process shell par vsftpd, il devient le PID 1 du container
-exec vsftpd /etc/vsftpd/vsftpd.conf
+exec vsftpd /etc/vsftpd/vsftpd.conf -obackground=NO
